bdb7a9782273f89bbe7062286be961ad747799a1
Steve <steve1.rm@gmail.com>
2015-11-17 23:26:09 +0700

    Added checking the buttons to show that they have been clicked

--- app/src/main/java/com/ssd/viber/MainActivity.java
+++ app/src/main/java/com/ssd/viber/MainActivity.java
@@ -2,12 +2,74 @@ package com.ssd.viber;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Boolean mChecked = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        final ImageView ivContacts = (ImageView)findViewById(R.id.ivContacts);
+        ivContacts.setTag(mChecked);
+
+        final ImageView ivMute = (ImageView)findViewById(R.id.ivMute);
+        final ImageView ivPause = (ImageView)findViewById(R.id.ivPause);
+        final ImageView ivSpeaker = (ImageView)findViewById(R.id.ivSpeaker);
+        final ImageView ivSwitchGsm = (ImageView)findViewById(R.id.ivSwitch);
+
+        final TextView tvContacts = (TextView)findViewById(R.id.tvContacts);
+        final View tblContact = findViewById(R.id.tbContacts);
+        ivContacts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mChecked) {
+                    ivContacts.setImageDrawable(getResources().getDrawable(R.drawable.ic_people_black_48dp));
+                    tvContacts.setTextColor(getResources().getColor(android.R.color.black));
+                    tblContact.setBackgroundColor(getResources().getColor(R.color.colorChecked));
+                    mChecked = false;
+                }
+                else {
+                    ivContacts.setImageDrawable(getResources().getDrawable(R.drawable.ic_people_white_48dp));
+                    tvContacts.setTextColor(getResources().getColor(android.R.color.white));
+                    tblContact.setBackgroundColor(getResources().getColor(R.color.colorDialer));
+                    mChecked = true;
+                }
+            }
+        });
+
+        ivMute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        ivPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        ivSpeaker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        ivSwitchGsm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
