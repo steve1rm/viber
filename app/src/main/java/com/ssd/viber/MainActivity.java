075aca9b8e622bdeb624866a2a9e584f4b837009
Steve <steve1.rm@gmail.com>
2015-11-23 21:18:19 +0700

    Added new icons for quality network indicator

--- app/src/main/java/com/ssd/viber/MainActivity.java
+++ app/src/main/java/com/ssd/viber/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ssd.viber;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,7 +8,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private Boolean mChecked = false;
+    private Boolean mChecked = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,29 +16,33 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        final ImageView ivContacts = (ImageView)findViewById(R.id.ivContacts);
-        ivContacts.setTag(mChecked);
+/*
+        final ImageView ivMute = (ImageView)findViewById(R.id.ivMute);
+        ivMute.setTag(mChecked);
+*/
 
         final ImageView ivMute = (ImageView)findViewById(R.id.ivMute);
         final ImageView ivPause = (ImageView)findViewById(R.id.ivPause);
         final ImageView ivSpeaker = (ImageView)findViewById(R.id.ivSpeaker);
         final ImageView ivSwitchGsm = (ImageView)findViewById(R.id.ivSwitch);
 
-        final TextView tvContacts = (TextView)findViewById(R.id.tvContacts);
-        final View tblContact = findViewById(R.id.tbContacts);
-        ivContacts.setOnClickListener(new View.OnClickListener() {
+        final TextView tvMute = (TextView)findViewById(R.id.tvMute);
+        final View tblContact = findViewById(R.id.tbMute);
+        ivMute.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(mChecked) {
-                    ivContacts.setImageDrawable(getResources().getDrawable(R.drawable.ic_people_black_48dp));
-                    tvContacts.setTextColor(getResources().getColor(android.R.color.black));
+                    ivMute.setImageDrawable(getResources().getDrawable(R.drawable.ic_mic_off_black_48dp));
+                    tvMute.setTextColor(getResources().getColor(android.R.color.black));
                     tblContact.setBackgroundColor(getResources().getColor(R.color.colorChecked));
                     mChecked = false;
                 }
                 else {
-                    ivContacts.setImageDrawable(getResources().getDrawable(R.drawable.ic_people_white_48dp));
-                    tvContacts.setTextColor(getResources().getColor(android.R.color.white));
-                    tblContact.setBackgroundColor(getResources().getColor(R.color.colorDialer));
+                    ivMute.setImageDrawable(getResources().getDrawable(R.drawable.ic_mic_off_white_48dp));
+                    tvMute.setTextColor(getResources().getColor(android.R.color.white));
+                    if(Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN) {
+                        tblContact.setBackground(getResources().getDrawable(R.drawable.outer_border));
+                    }
                     mChecked = true;
                 }
             }
@@ -70,6 +75,5 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
     }
 }
